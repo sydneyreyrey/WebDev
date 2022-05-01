1276cf22ba0f89fe75ad1756cc5f8ad50a6b7391
sydneyreyrey <92216740+sydneyreyrey@users.noreply.github.com>
2022-05-01 17:07:17 -0500

    here

--- Web Dev/HW4/E4.js
+++ Web Dev/HW4/E4.js
@@ -7,18 +7,20 @@ function sug_act() {
 
     fetchBtn.addEventListener("click", buttonclickhandler);
     let ld = false;
-
     let res_obj = null;
   
     function buttonclickhandler() {
   
         // Instantiate an new XHR Object
         const req = new XMLHttpRequest();
+        
 
         let liDiv = document.createElement('div');
         let like = document.createElement('button');
         like.setAttribute('id', 'lbt');
         like.textContent = 'Like';
+        
+        
 
         let diDiv = document.createElement('div');
         let dislike = document.createElement('button');
@@ -26,9 +28,10 @@ function sug_act() {
         dislike.textContent = 'Dislike';
         
         req.onload = function() {
-            res_obj = JSON.parse(this.responseText);
-            console.log(res_obj)
-            document.getElementById("Suggested Activity").innerHTML = res_obj.activity
+            res_obj = JSON.parse(this.response);
+            document.getElementById("Suggested Activity").innerHTML = res_obj.activity;
+            like.addEventListener("click", Like);
+
         };
 
         req.open("GET", "https://www.boredapi.com/api/activity", true);
@@ -47,28 +50,50 @@ function sug_act() {
   
     }
 
+    
+    let tblstat = false;
+
     function Like() {
+        
+        
         let tbl = document.createElement('table');
-        let tblBody = document.createElement('tbody');
-        tbl.appendChild(tblBody);
-        let tblstat = false;
+        tbl.setAttribute('id', 'tbl');
+        //let tblBody = document.createElement('tbody');
+        //tbl.appendChild(tblBody);
+        let tblHead = document.createElement('th');
+        tbl.appendChild(tblHead);
+
+        let info = ['activity', 'type', 'participants', 'price', 'accesibility'];
+
+        
 
         if (!tblstat) {
             let lktbl = document.getElementById('like_table');
             lktbl.appendChild(tbl);
+            info.forEach(element => {
+                let cell = document.createElement('td');
+                cell.setAttribute('id', 'cell')
+                console.log(element)
+                cell.textContent = element;
+                tblHead.appendChild(cell);
+            });
+            tblstat = true;
         }
 
-        let tbl_stuff = ['activity', 'type', 'participants', 'price', 'accesibility'];
-        console.log('here');
-        console.log(res_obj);
-        let row = doucument.createElement('tr');
-        for (i = 0; i < 5; i++) {
+        
+
+        let row = document.createElement('tr');
+        info.forEach(element => {
             let cell = document.createElement('td');
-            cell.textContent = null;
+            cell.setAttribute('id', 'cell')
+            cell.textContent = res_obj[element];
+            row.appendChild(cell);
 
             //row.appendChild()
-        }
+        });
+        tbl.appendChild(row);
     }
+    
 
 }
 
